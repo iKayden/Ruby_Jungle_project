71ab09c48821176799bfb615210c4806aaadc46a
ikayden <ikyden23@gmail.com>
2022-11-16 20:34:08 -0800

    Fix top nav. Add new category page, Finish Task

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+
   get "/about", to: "about#index"
+  get '/rails/info/routes' => 'routes'
 
   resource :cart, only: [:show] do
     post   :add_item
