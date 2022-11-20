49fba99564c75061852234867a2f38acf35bf4e2
ikayden <ikyden23@gmail.com>
2022-11-19 17:19:51 -0800

    Add more password and new email specs

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
 
   describe "#password" do
@@ -38,10 +37,49 @@ RSpec.describe User, type: :model do
         # no password
       )
       @user.save
+
       expect(@user).not_to be_valid
-      expect(@user.errors.full_messages).to eq(["Password can't be blank", "Password confirmation can't be blank", "Password can't be blank"])
+      expect(@user.errors.full_messages).to eq(["Password can't be blank", "Password is too short (minimum is 5 characters)", "Password confirmation can't be blank", "Password can't be blank"])
     end
 
+    it "needs to require password longer than 5 characters" do
+      @user = User.new(
+        first_name: "Bob",
+        last_name: "Ross",
+        email: "bobby1985@gmail.com",
+        password: "1234",
+        password_confirmation: "1234"
+      )
+      @user.save
 
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to eq(["Password is too short (minimum is 5 characters)"])
+    end
+  end
+
+  describe "#email" do
+    it "needs forbid a new user to register if an email already exists in our data base" do
+      @user_old = User.new(
+        first_name: "Bob",
+        last_name: "Ross",
+        email: "bobby1985@gmail.com",
+        password: "qwerty",
+        password_confirmation: "qwerty"
+      )
+      @user_old.save
+
+      @user_new = User.new(
+        first_name: "Bobby",
+        last_name: "Decartes",
+        email: "bobby1985@gmail.com",
+        password: "qwerty",
+        password_confirmation: "qwerty"
+      )
+      @user_new.save
+
+      expect(@user_old).to be_valid
+      expect(@user_new).not_to be_valid
+      expect(@user_new.errors.full_messages).to eq(["Email has already been taken"])
+    end
   end
 end
