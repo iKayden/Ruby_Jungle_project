719d48d0ccd89f8ca7a136c34a981ef603ae6557
ikayden <ikyden23@gmail.com>
2022-11-20 08:46:13 -0800

    Add User Signup Test

---
+++ cypress/integration/jungle-tests/4.user-login.spec.js
@@ -0,0 +1,36 @@
+describe('User Sign Up', () => {
+
+  beforeEach(() => {
+    // cy.exec("rake reset:db");
+    cy.exec('rake db:reset RAILS_ENV=test');
+    cy.visit('/');
+  });
+
+  it("can sign up a new user", () => {
+    cy.contains("Signup")
+      .click({ force: true });
+
+    // filling up the signup form
+    cy.get("#user_first_name")
+      .type("Kayden");
+    cy.get("#user_last_name")
+      .type("Khar");
+    cy.get("#user_email")
+      .type("Kayden@gmail.com");
+    cy.get("#user_password")
+      .type("qwerty");
+    cy.get("#user_password_confirmation")
+      .type("qwerty");
+
+    cy.contains("Submit")
+      .click({ force: true });
+
+    cy.contains("Signed in as");
+
+  });
+
+  // after(() => {
+
+  //   cy.exec('db:reset RAILS_ENV=test');
+  // });
+});
