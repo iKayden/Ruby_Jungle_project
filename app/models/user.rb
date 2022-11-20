85af60e24f3e3b37d0e6299e385def269b6acff0
ikayden <ikyden23@gmail.com>
2022-11-19 17:03:09 -0800

    Add pass min length and new auth class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,28 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: {minimum: 5}
   validates :password_confirmation, presence: true
   validates_uniqueness_of :email, :case_sensetive => false
 
   has_secure_password
+
+
+  def authenticate_with_credentials(email, password)
+    email = email.strip.downcase unless email.nil?
+    user = self.find_by_email(email)
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 
   private
 
   def lowercase_emails
     self.email = email.downcase
   end
-
 
 end
